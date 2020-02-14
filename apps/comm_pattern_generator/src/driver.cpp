231acb9ff30c8d6d37ff94ed754d2d4cc99c4425
Dylan Chapp <dchapp@udel.edu>
2020-02-13 22:38:06 -0500

    Basic functionality working for two example communication patterns

--- apps/comm_pattern_generator/src/driver.cpp
+++ apps/comm_pattern_generator/src/driver.cpp
@@ -1,9 +1,12 @@
 #include <mpi.h>
 #include <cstdio>
 #include <cstdlib>
+#include <string>
+#include <iostream>
 
 #include "naive_reduce.hpp"
 #include "amg2013.hpp"
+#include "configuration.hpp"
 
 int main( int argc, char** argv )
 {
@@ -12,24 +15,48 @@ int main( int argc, char** argv )
   mpi_rc = MPI_Comm_rank( MPI_COMM_WORLD, &rank );
   mpi_rc = MPI_Comm_size( MPI_COMM_WORLD, &comm_size );
 
-  int color;
-  if ( rank < 4 ) {
-    color = 0;
+  // Ingest configuration
+  Configuration config;
+  if ( rank == 0 ) {
+    std::string config_path( argv[1] );
+    config = Configuration(config_path);
+    broadcast_config( config );
   } else {
-    color = 1;
+    broadcast_config( config );
   }
-
-  MPI_Comm pattern_comm;
-  mpi_rc = MPI_Comm_split( MPI_COMM_WORLD, color, rank, &pattern_comm);
-
-  int pattern_comm_rank, pattern_comm_size;
-  mpi_rc = MPI_Comm_rank( pattern_comm, &pattern_comm_rank );
-  mpi_rc = MPI_Comm_size( pattern_comm, &pattern_comm_size );
-
-  //comm_pattern_naive_reduce( pattern_comm, color );
-  comm_pattern_amg2013( pattern_comm, color, false );
-
-  mpi_rc = MPI_Comm_free( &pattern_comm );
+  
+  // Iterate over the comm patterns
+  auto comm_pattern_seq = config.get_comm_pattern_seq();
+  for ( auto comm_pattern : comm_pattern_seq ) {
+    // First, split comms based on desired fraction of non-deterministic 
+    // communication
+    auto nd_fraction = comm_pattern.nd_fraction;
+    int cutoff_rank = comm_size * nd_fraction;
+    if ( rank == 0 ) {
+      std::cout << "Ranks up to " << cutoff_rank << " will execute non-deterministic version of pattern" << std::endl;
+    }
+    int color;
+    if ( rank < cutoff_rank ) {
+      color = 0;
+    } else {
+      color = 1;
+    }
+    MPI_Comm pattern_comm;
+    mpi_rc = MPI_Comm_split( MPI_COMM_WORLD, color, rank, &pattern_comm);
+    // Do this pattern for specified number of iterations
+    auto n_iters = comm_pattern.n_iters;
+    for ( int i=0; i<n_iters; ++i ) {
+      auto pattern_name = comm_pattern.pattern_name;
+      if ( pattern_name == "naive_reduce" ) {
+        comm_pattern_naive_reduce( pattern_comm, color );
+      } 
+      else if ( pattern_name == "amg2013" ) {
+        comm_pattern_amg2013( pattern_comm, color, false );
+      }
+      mpi_rc = MPI_Barrier( MPI_COMM_WORLD );
+    }
+    mpi_rc = MPI_Comm_free( &pattern_comm );
+  }
 
   mpi_rc = MPI_Finalize();
   return EXIT_SUCCESS;
