e8a9ccd8a08c172bdbb101974f4430bf6420ffbd
Dylan Chapp <dchapp@udel.edu>
2020-02-26 12:44:16 -0800

    Updated build scripts to build in system-specific build directories

--- install/install_dumpi.sh
+++ install/install_dumpi.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+system=$( hostname | sed 's/[0-9]*//g' )
+build_dir=build/${system}
+
 cd submodules/sst-dumpi
-mkdir -p build
+mkdir -p ${build_dir}
 export CC=mpicc
 export CXX=mpicxx
 $(pwd)/bootstrap.sh
-$(pwd)/configure --prefix=$(pwd)/build --enable-libdumpi 
+$(pwd)/configure --prefix=$(pwd)/${build_dir} --enable-libdumpi 
 make -j
 make install
--- install/install_pnmpi.sh
+++ install/install_pnmpi.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+system=$( hostname | sed 's/[0-9]*//g' )
+build_dir=build/${system}
+
 cd submodules/PnMPI
 git submodule update --init --recursive
-mkdir -p build
-cd build
+mkdir -p ${build_dir}
+cd ${build_dir}
 export CC=mpicc
 export CXX=mpicxx
 cmake -DCMAKE_INSTALL_PREFIX=$(pwd) -DENABLE_FORTRAN=OFF ..
