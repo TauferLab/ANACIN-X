5f1ad994569adbd40b9291c621ee3dd1bca6ad9b
Dylan Chapp <dchapp@udel.edu>
2020-02-25 19:03:12 -0800

    Remove obsolete fn decls

--- apps/comm_pattern_generator/include/comm_pattern_generator/amg2013.hpp
+++ apps/comm_pattern_generator/include/comm_pattern_generator/amg2013.hpp
@@ -4,7 +4,5 @@
 #include <mpi.h>
 
 void comm_pattern_amg2013( int iter, double nd_fraction, int msg_size );
-//void comm_pattern_amg2013_non_deterministic( MPI_Comm comm, bool randomize_initial_sends );
-//void comm_pattern_amg2013_deterministic( MPI_Comm comm );
 
 #endif
