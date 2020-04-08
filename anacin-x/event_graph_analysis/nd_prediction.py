ec8454a4b344fb78d401cbe4971774c2839a57a8
Dylan Chapp <dchapp@udel.edu>
2020-04-07 23:16:41 -0700

    Update graph kernel SVR

--- anacin-x/event_graph_analysis/nd_prediction.py
+++ anacin-x/event_graph_analysis/nd_prediction.py
@@ -30,16 +30,78 @@ def get_label( slice_path, nd_fraction_labels ):
 
 def label_slices( traces_root_dir ):
     #nd_fraction_labels = [ 0, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0 ]
-    nd_fraction_labels = [ 0, 20, 30, 40, 50, 60, 70, 80, 90, 100 ]
+    nd_fraction_labels = [ 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 ]
     slice_dirs = glob.glob( traces_root_dir + "/run*/slices/" )
     slice_path_to_label = {}
     for sd in slice_dirs:
         slices = sorted( glob.glob( sd + "/*.graphml" ) )
         for s in slices:
-            slice_path_to_label[ s ] = get_label( s, nd_fraction_labels )
+            nd_fraction = get_label( s, nd_fraction_labels )
+            slice_path_to_label[ s ] = nd_fraction
+            #if nd_fraction < 50:
+            #    slice_path_to_label[ s ] = 0
+            #else:
+            #    slice_path_to_label[ s ] = 1
     return slice_path_to_label
 
+"""
+Load GraKeL base graph kernel definitions/parameters from config file
+"""
+def load_base_kernels(base_kernels_path="grakel_base_kernels.json"):
+    with open(base_kernels_path, "r") as infile:
+        base_kernels = json.load(infile)
+    return base_kernels
+
+
+"""
+Construct Weisfeiler-Lehman versions of all base kernels
+"""
+def define_wl_kernels(base_kernels, wl_iters=10):
+    wl_kernels = {}
+    for name,base_kernel in base_kernels.items():
+        key = name + "_wl" 
+        # Override base kernel's vertex label requirements
+        constraints = base_kernel["constraints"]
+        constraints["vertex_label"] = "needs"
+        base_params = base_kernel["params"]
+        wl_kernels[key] = {"constraints" : constraints, "params" : [{"name" : "weisfeiler_lehman", "n_iter" : wl_iters}, base_params]}
+    return wl_kernels
+
+"""
+Construct Hadamard Code versions of all base kernels
+"""
+def define_hc_kernels(base_kernels):
+    hadamard_code_kernels = {}
+    for name,base_kernel in base_kernels.items():
+        key = name + "_hc" 
+        # Override base kernel's vertex label requirements
+        constraints = base_kernel["constraints"]
+        constraints["vertex_label"] = "needs"
+        base_params = base_kernel["params"]
+        hadamard_code_kernels[key] = {"constraints" : constraints, "params" : [{"name" : "hadamard_code"}, base_params]}
+    return hadamard_code_kernels
+                   
+
+"""
+Construct Core Framework versions of all base kernels
+"""
+def define_cf_kernels(base_kernels):
+    core_framework_kernels = {}
+    for name,base_kernel in base_kernels.items():
+        key = name + "_cf" 
+        constraints = base_kernel["constraints"]
+        base_params = base_kernel["params"]
+        core_framework_kernels[key] = {"constraints" : constraints, "params" : [{"name" : "core_framework"}, base_params]}
+    return core_framework_kernels
+
+
+
 def main( traces_root_dir, kernel_file ):
+    base_kernels = load_base_kernels()
+    wl_kernels = define_wl_kernels(base_kernels)
+    hc_kernels = define_hc_kernels(base_kernels)
+    cf_kernels = define_cf_kernels(base_kernels)
+
     # Read in kernel params 
     with open( kernel_file, "r" ) as infile:
         kernels = json.load( infile )["kernels"]
@@ -67,7 +129,7 @@ def main( traces_root_dir, kernel_file ):
         # Compute graph kernel
         g_train = [ grakel_graphs[i] for i in train_indices ]
         g_test = [ grakel_graphs[i] for i in test_indices ]
-        kernel = [{"name": "weisfeiler_lehman", "n_iter": 5}, 
+        kernel = [{"name": "weisfeiler_lehman", "n_iter": 10}, 
                   {"name": "subtree_wl"}]
         gk = GraphKernel(kernel=kernel, normalize=True)
         k_train = gk.fit_transform( g_train )
@@ -75,14 +137,26 @@ def main( traces_root_dir, kernel_file ):
         
         #print( k_train.shape )
         #print( k_test.shape )
+        #exit()
         
         # Train classifier
         y_train = [ labels[i] for i in train_indices ]
         y_test = [ labels[i] for i in test_indices ]
-        clf = svm.SVC()
-        clf.fit(k_train, y_train)
-        y_pred = clf.predict(k_test)
-        #print(str(round(accuracy_score(y_test, y_pred)*100, 2)), "%")
+        #clf = svm.SVC()
+        #clf.fit(k_train, y_train)
+        #y_pred = clf.predict(k_test)
+        reg = svm.SVR()
+        reg.fit(k_train, y_train)
+        y_pred = reg.predict(k_test)
+        
+        rmse = 0
+        for true,pred in zip(y_test,y_pred):
+            rmse += (true - pred) ** 2
+            print("True: {}, Predicted: {}".format(true,pred))
+        rmse = np.sqrt(rmse / len(y_test))
+        print("RMSE: {}".format(rmse))
+        exit()
+
         accuracy = accuracy_score(y_test, y_pred)
         print("Round {}/100: accuracy = {}".format(i+1, accuracy))
         scores.append( accuracy )
