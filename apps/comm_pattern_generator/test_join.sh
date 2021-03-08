4f449851a21591a660cef355d0b9d2557313d688
Kae Suarez <dsuarez1423@gmail.com>
2021-03-08 10:26:19 -0500

    Updating record of work with job-string joining.

--- apps/comm_pattern_generator/test_join.sh
+++ apps/comm_pattern_generator/test_join.sh
@@ -5,5 +5,5 @@ kdts_job_deps+=("1234")
 kdts_job_deps+=("5678")
 
 function join_by { local IFS="$1"; shift; echo "$*"; }
-kdts_job_dep_str=$( join_by : ${kdts_job_deps[@]} )
+kdts_job_dep_str=$( join_by '&&' ${kdts_job_deps[@]} )
 echo ${kdts_job_dep_str}
