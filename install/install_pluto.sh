80add6aff317c99a4148cef91e46ac7b9821f4e0
Kae Suarez <dsuarez1423@gmail.com>
2021-03-12 14:11:58 -0500

    Actually build Pluto with ANACIN.

---
+++ install/install_pluto.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd submodules/Src_Pluto
+$(pwd)/one_step_build.sh
--- setup.sh
+++ setup.sh
@@ -38,6 +38,19 @@ echo
 
 echo
 echo ${progress_delimiter}
+echo "Building Pluto..."
+echo ${progress_delimiter}
+echo
+./install/install_pluto.sh
+echo 
+echo ${progress_delimiter}
+echo "Done building Pluto."
+echo ${progress_delimiter}
+echo
+
+
+echo
+echo ${progress_delimiter}
 echo "Building NINJA Noise-Injection Agent..."
 echo ${progress_delimiter}
 echo
@@ -78,9 +91,11 @@ echo ${progress_delimiter}
 echo "Patching tracing libraries for use with PnMPI..."
 echo ${progress_delimiter}
 echo 
-./submodules/PnMPI/build_tellico-master/bin/pnmpi-patch ./submodules/sst-dumpi/build_tellico-master/lib/libdumpi.so ./anacin-x/pnmpi/patched_libs/libdumpi.so
-./submodules/PnMPI/build_tellico-master/bin/pnmpi-patch ./submodules/csmpi/build/libcsmpi.so ./anacin-x/pnmpi/patched_libs/libcsmpi.so
-./submodules/PnMPI/build_tellico-master/bin/pnmpi-patch ./submodules/ninja/build/libninja.so ./anacin-x/pnmpi/patched_libs/libninja.so
+./submodules/PnMPI/build/bin/pnmpi-patch ./submodules/sst-dumpi/build/lib/libdumpi.so ./anacin-x/pnmpi/patched_libs/libdumpi.so
+./submodules/PnMPI/build/bin/pnmpi-patch ./submodules/csmpi/build/libcsmpi.so ./anacin-x/pnmpi/patched_libs/libcsmpi.so
+./submodules/PnMPI/build/bin/pnmpi-patch ./submodules/ninja/build/libninja.so ./anacin-x/pnmpi/patched_libs/libninja.so
+./submodules/PnMPI/build/bin/pnmpi-patch ./submodules/Src_Pluto/build/libpluto.so ./anacin-x/pnmpi/patched_libs/libpluto.so
+
 echo
 echo ${progress_delimiter}
 echo "Done patching tracing libraries for use with PnMPI..."
