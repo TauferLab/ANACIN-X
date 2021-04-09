9edd7b26ada347930c0c976d3a2819c243aceb61
Kae Suarez <dsuarez1423@gmail.com>
2021-04-09 15:00:51 -0400

    Further removing traces of mini_mcb

--- apps/comm_pattern_generator/config/json_gen.py
+++ apps/comm_pattern_generator/config/json_gen.py
@@ -9,7 +9,6 @@ out = {"comm_patterns": []}
 # recommended n_iters per type
 # amg2013:           1
 # msg_race:          10
-# mini_mcb:          1
 # unstructured_mesh: 1
 
 if sys.argv[1] == "amg2013":
@@ -42,35 +41,6 @@ elif sys.argv[1] == "naive_reduce":
                                    ]})
         nd_frac += 0.1
     with open("../config/message_race_msg_size_{}_niters_{}.json".format(int(sys.argv[2]), int(sys.argv[3])), 'w+') as f:
-        json.dump(out, f, indent=4)
-
-
-elif sys.argv[1] == "mini_mcb":
-    nd_frac = 0.0
-    for i in range(0, 11):
-        out["comm_patterns"].append({"pattern_name": sys.argv[1],
-                                   "n_iters": int(sys.argv[3]),
-                                   "nd_fraction": round(nd_frac, 1),
-                                   "params": [
-                                       {
-                                           "key": "n_sub_iters",
-                                           "val": "10"
-                                       },
-                                       {
-                                           "key": "n_grid_steps",
-                                           "val": "4"
-                                       },
-                                       {
-                                           "key": "n_reduction_steps",
-                                           "val": "4"
-                                       },
-                                       {
-                                           "key": "interleave_nd_iters",
-                                           "val": sys.argv[2]
-                                       }
-                                   ]})
-        nd_frac += 0.1
-    with open("../config/mini_mcb_{}_niters_{}.json".format(("interleaved" if (int(sys.argv[2]) == 1) else "non_interleaved"), int(sys.argv[3])), 'w+') as f:
         json.dump(out, f, indent=4)
 
 
