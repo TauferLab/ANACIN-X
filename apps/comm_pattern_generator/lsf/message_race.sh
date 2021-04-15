a7c03a0ed81657bc695ccea63ac42cd8c3267c24
Nick Bell <pnbell@tellico-master0.local>
2021-04-15 14:42:27 -0400

    lsf comm patterns to submit jobs by run

--- apps/comm_pattern_generator/lsf/message_race.sh
+++ apps/comm_pattern_generator/lsf/message_race.sh
@@ -4,69 +4,50 @@ n_procs=$1
 n_iters=$2
 msg_size=$3
 n_nodes=$4
-run_idx_low=$5
-run_idx_high=$6
-results_root=$7
+run_idx=$5
+example_paths_dir=$6
+run_dir=$7
+#debugging_path=$8
 
-source ./example_paths_lsf.config
-example_paths_dir=$(pwd)
+source ${example_paths_dir}/example_paths_lsf.config
+#echo ${example_paths_dir}
+#example_paths_dir=$(pwd)
 
-#message_sizes=(1 2 4 8 16 32 64 128 256 512 1024 2048)
-#message_sizes=(1 512 1024 2048)
-#message_sizes=(512)
+#function join_by { local d=$1; shift; local f=$1; shift; printf %s "$f" "${@/#/$d}"; }
 
-#rm -r ${results_root}/*
-
-# Non-NINJA Workflow
-#for iters in ${n_iters[@]};
+#for run_idx in `seq -f "%03g" ${run_idx_low} ${run_idx_high}`; 
 #do
-#    for procs in ${n_procs[@]};
-#    do
-#	for msg_size in ${message_sizes[@]};
-#	do
-for run_idx in `seq -f "%03g" ${run_idx_low} ${run_idx_high}`; 
-do
 
-    # Set up paths
-    run_dir=${results_root}/msg_size_${msg_size}/nprocs_${n_procs}/niters_${n_iters}/without_ninja/run_${run_idx}
-    mkdir -p ${run_dir}
-    debugging_path=${run_dir}/debug
-    mkdir -p ${debugging_path}
-    app_config=${anacin_x_root}/apps/comm_pattern_generator/config/message_race_msg_size_${msg_size}_niters_${n_iters}.json
-    #app_config=${anacin_x_root}/apps/comm_pattern_generator/config/message_race_msg_size_${msg_size}.json
-    
-    # Create app config if doesn't exist
-    if [ ! -f "$app_config" ]; then
-	python3 > ${debugging_path}/create_json_output.txt 2> ${debugging_path}/create_json_error.txt ${anacin_x_root}/apps/comm_pattern_generator/config/json_gen.py "naive_reduce" ${msg_size} ${n_iters} "${example_paths_dir}/../"
-    fi
+# Set up paths
+#run_dir=${results_root}/msg_size_${msg_size}/nprocs_${n_procs}/niters_${n_iters}/without_ninja/run_${run_idx}
+#mkdir -p ${run_dir}
+debugging_path=${run_dir}/debug
+mkdir -p ${debugging_path}
+app_config=${anacin_x_root}/apps/comm_pattern_generator/config/message_race_msg_size_${msg_size}_niters_${n_iters}.json
+#app_config=${anacin_x_root}/apps/comm_pattern_generator/config/message_race_msg_size_${msg_size}.json
 
-    echo ${app_config}
-    # Trace execution
-    LD_PRELOAD=${pnmpi} PNMPI_LIB_PATH=${pnmpi_lib_path} PNMPI_CONF=${pnmpi_conf} mpirun -np ${n_procs} > ${debugging_path}/trace_exec_output.txt 2> ${debugging_path}/trace_exec_error.txt ${app_bin} ${app_config}
-    mv dumpi-* ${run_dir}
-    mv pluto_out* ${run_dir}
+# Create app config if doesn't exist
+if [ ! -f "$app_config" ]; then
+    python3 > ${debugging_path}/create_json_output.txt 2> ${debugging_path}/create_json_error.txt ${anacin_x_root}/apps/comm_pattern_generator/config/json_gen.py "naive_reduce" ${msg_size} ${n_iters} "${example_paths_dir}/../"
+fi
 
-    # Build event graph
-    mpirun -np ${n_procs} > ${debugging_path}/build_graph_output.txt 2> ${debugging_path}/build_graph_error.txt ${dumpi_to_graph_bin} ${dumpi_to_graph_config} ${run_dir}
-    event_graph=${run_dir}/event_graph.graphml
+echo ${app_config}
+# Trace execution
+LD_PRELOAD=${pnmpi} PNMPI_LIB_PATH=${pnmpi_lib_path} PNMPI_CONF=${pnmpi_conf} mpirun -np ${n_procs} > ${debugging_path}/trace_exec_output.txt 2> ${debugging_path}/trace_exec_error.txt ${app_bin} ${app_config}
+mv dumpi-* ${run_dir}
+mv pluto_out* ${run_dir}
 
-    # Extract slices
-    mpirun -np ${n_procs} > ${debugging_path}/extract_slices_output.txt 2> ${debugging_path}/extract_slices_error.txt ${extract_slices_script} ${event_graph} ${slicing_policy} -o "slices"
+# Build event graph
+mpirun -np ${n_procs} > ${debugging_path}/build_graph_output.txt 2> ${debugging_path}/build_graph_error.txt ${dumpi_to_graph_bin} ${dumpi_to_graph_config} ${run_dir}
+event_graph=${run_dir}/event_graph.graphml
 
-    cp ${event_graph} ${results_root}/../comm_pattern_graphs/graph_message_race_niters_${n_iters}_nprocs_${n_procs}_msg_size_${msg_size}_run_${run_idx}.graphml
-    mkdir -p ${anacin_x_root}/strong_scaling_tests
-    #for slice_name in $(seq 0 10); do
-	#cp ${run_dir}/slices/slice_${slice_name}.graphml ${anacin_x_root}/strong_scaling_tests/slice_${slice_name}_message_race_niters_${n_iters}_nprocs_${n_procs}_msg_size_${msg_size}_run_${run_idx}.graphml
-    #done
-    #mkdir -p ${anacin_x_root}/weak_scaling_tests
-    #cp ${run_dir}/slices/slice_2.graphml ${anacin_x_root}/weak_scaling_tests/slice2_message_race_niters_${n_iters}_nprocs_${n_procs}_msg_size_${msg_size}_run_${run_idx}.graphml
+# Extract slices
+mpirun -np ${n_procs} > ${debugging_path}/extract_slices_output.txt 2> ${debugging_path}/extract_slices_error.txt ${extract_slices_script} ${event_graph} ${slicing_policy} -o "slices"
 
-done
+
+#done
 
 # Compute KDTS
 #mpirun -np 10 > ${debugging_path}/compute_kdts_output.txt 2> ${debugging_path}/compute_kdts_error.txt ${compute_kdts_script} "${results_root}/msg_size_${msg_size}/without_ninja/" ${graph_kernel} --slice_dir_name "slices" -o "kdts.pkl"
-mpirun -np ${n_procs} > ${debugging_path}/../../compute_kdts_output.txt 2> ${debugging_path}/../../compute_kdts_error.txt ${compute_kdts_script} "${run_dir}/../" ${graph_kernel} --slice_dir_name "slices" -o "kdts.pkl"
+#mpirun -np ${n_procs} > ${debugging_path}/../../compute_kdts_output.txt 2> ${debugging_path}/../../compute_kdts_error.txt ${compute_kdts_script} "${run_dir}/../" ${graph_kernel} --slice_dir_name "slices" -o "kdts.pkl"
 
-#	done
-#    done
-#done
--- apps/comm_pattern_generator/lsf/message_race_lsf.sh
+++ apps/comm_pattern_generator/lsf/message_race_lsf.sh
@@ -9,10 +9,33 @@ run_idx_high=$6
 results_root=$7
 
 source ./example_paths_lsf.config
+example_paths_dir=$(pwd)
 
-message_race_script=${anacin_x_root}/apps/comm_pattern_generator/lsf/message_race.sh
+function join_by { local d=$1; shift; local f=$1; shift; printf %s "$f" "${@/#/$d}"; }
+kdts_job_deps=()
 
+comm_pattern_job_script=${anacin_x_root}/apps/comm_pattern_generator/lsf/message_race.sh
 n_procs_per_node=$((n_procs/n_nodes))
 
-#bsub -n ${n_procs} ${message_race_script} ${n_procs} ${n_iters} ${msg_size} ${run_idx_low} ${run_idx_high} ${results_root}
-bsub -n ${n_procs} -R "span[ptile=${n_procs_per_node}]" -o ${debugging_path}/lsf_output.txt -e ${debugging_path}/lsf_error.txt ${message_race_script} ${n_procs} ${n_iters} ${msg_size} ${n_nodes} ${run_idx_low} ${run_idx_high} ${results_root}
+for run_idx in `seq -f "%03g" ${run_idx_low} ${run_idx_high}`;
+do
+
+    # Set up paths
+    run_dir=${results_root}/msg_size_${msg_size}/nprocs_${n_procs}/niters_${n_iters}/without_ninja/run_${run_idx}
+    mkdir -p ${run_dir}
+    debugging_path=${run_dir}/debug
+    mkdir -p ${debugging_path}
+    cd ${run_dir}
+
+    #bsub -n ${n_procs} ${message_race_script} ${n_procs} ${n_iters} ${msg_size} ${run_idx_low} ${run_idx_high} ${results_root}
+    comm_pattern_run_stdout=$( bsub -n ${n_procs} -R "span[ptile=${n_procs_per_node}]" -o ${debugging_path}/lsf_output.txt -e ${debugging_path}/lsf_error.txt ${comm_pattern_job_script} ${n_procs} ${n_iters} ${msg_size} ${n_nodes} ${run_idx} ${example_paths_dir} ${run_dir} )
+    comm_pattern_job_id=$( echo ${comm_pattern_run_stdout} | sed 's/[^0-9]*//g' )
+    kdts_job_deps+=("done(${comm_pattern_job_id})")
+    echo ${kdts_job_deps} 
+
+done
+
+kdts_job_dep_str=$( join_by "&&" ${kdts_job_deps[@]} )
+echo ${kdts_job_dep_str}
+cd ${run_dir}/../
+bsub -n ${n_procs} -R "span[ptile=${n_procs_per_node}]" -w ${kdts_job_dep_str} -o compute_kdts_output.txt -e compute_kdts_error.txt ${compute_kdts_script} "${run_dir}/../" ${graph_kernel} --slice_dir_name "slices" -o "kdts.pkl"
